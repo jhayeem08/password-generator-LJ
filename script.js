9e9e67c6d6ea12980b7cf63cf725b63f3f832d32
James Damisel <James's Email>
2023-06-18 19:55:53 +1000

    Included characters and lower case characters

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword(){
+function generatePassword() {
   console.log("inside generate password");
 
   let passwordLength = parseInt(prompt("please give password length between 8 and 128"));
-  if (Number.isNaN(passwordLength)){
+  if (Number.isNaN(passwordLength)) {
     alert("password length must a number");
-    return("please retry with proper length");
+    return ("please retry with proper length");
   }
 
-  if (passwordLength < 8){
+  if (passwordLength < 8) {
     alert("password length must be atleast 8 characters");
-    return("please retry with length between 8 and 128");
+    return ("please retry with length between 8 and 128");
   }
 
-  if (passwordLength > 128){
+  if (passwordLength > 128) {
     alert("password length must be 128 or less characters");
-    return("please retry with length between 8 and 128");
+    return ("please retry with length between 8 and 128");
   }
 
-let lower = confirm("press OK to include lower case characters in your password. Cancel otherwise");
+  let includedCharacters = "";
+  let lower = confirm("press OK to include lower case characters in your password. Cancel otherwise");
+  let lowerCharacters = "abcdefghijklmnopqrstuvwxyz";
 
+  if (lower){
+    includedCharacters += lowerCharacters;
+  }
+  console.log("included characters = ", includedCharacters);
 }
 
 // Write password to the #password input
